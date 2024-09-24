3598b6eafc0a2ed5e7116e9c40c8e180c70ba06c
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-09-24 03:15:47 -0300

    comment out og generation for now

--- astro.config.ts
+++ astro.config.ts
@@ -4,29 +4,24 @@ import UnoCSS from "unocss/astro";
 import react from "@astrojs/react";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === "production"
-      ? "https://brutal.elian.codes/"
-      : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}/`
-        : "https://localhost:3000/",
+  site: process.env.VERCEL_ENV === "production" ? "https://brutal.elian.codes/" : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : "https://localhost:3000/",
   trailingSlash: "ignore",
-  integrations: [
-    sitemap(),
-    UnoCSS({
-      injectReset: true,
-    }),
-    react(),
-  ],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), react()],
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ["@resvg/resvg-js"]
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -1,59 +1,59 @@
-import { Resvg, type ResvgRenderOptions } from "@resvg/resvg-js";
-import type { APIRoute } from "astro";
-import satori from "satori";
-import { html as toReactElement } from "satori-html";
+// import { Resvg, type ResvgRenderOptions } from "@resvg/resvg-js";
+// import type { APIRoute } from "astro";
+// import satori from "satori";
+// import { html as toReactElement } from "satori-html";
 
-const fontFile = await fetch(
-  "https://og-playground.vercel.app/inter-latin-ext-700-normal.woff"
-);
+// const fontFile = await fetch(
+//   "https://og-playground.vercel.app/inter-latin-ext-700-normal.woff"
+// );
 
-const fontData: ArrayBuffer = await fontFile.arrayBuffer();
+// const fontData: ArrayBuffer = await fontFile.arrayBuffer();
 
-const height = 630;
-const width = 1200;
+// const height = 630;
+// const width = 1200;
 
-export const GET: APIRoute = async () => {
-  const html = toReactElement(`
-  <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
-    <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
-      <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
-        <div style="display: flex; flex-direction: column; gap: 0.75rem;">
-          <p style="font-size: 48px;">Kevin Ruhl Website</p>
-        </div>
-        <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
-          <img src="" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
-        </div>
-      </div>
-    </div>
-  </div>
-  `);
+// export const GET: APIRoute = async () => {
+//   const html = toReactElement(`
+//   <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
+//     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
+//       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
+//         <div style="display: flex; flex-direction: column; gap: 0.75rem;">
+//           <p style="font-size: 48px;">Kevin Ruhl Website</p>
+//         </div>
+//         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
+//           <img src="" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   `);
 
-  const svg = await satori(html, {
-    fonts: [
-      {
-        name: "Inter Latin",
-        data: fontData,
-        style: "normal",
-      },
-    ],
+//   const svg = await satori(html, {
+//     fonts: [
+//       {
+//         name: "Inter Latin",
+//         data: fontData,
+//         style: "normal",
+//       },
+//     ],
 
-    height,
-    width,
-  });
+//     height,
+//     width,
+//   });
 
-  const opts: ResvgRenderOptions = {
-    fitTo: {
-      mode: "width",
-      value: width,
-    },
-  };
-  const resvg = new Resvg(svg, opts);
-  const pngData = resvg.render();
-  const pngBuffer = pngData.asPng();
+//   const opts: ResvgRenderOptions = {
+//     fitTo: {
+//       mode: "width",
+//       value: width,
+//     },
+//   };
+//   const resvg = new Resvg(svg, opts);
+//   const pngData = resvg.render();
+//   const pngBuffer = pngData.asPng();
 
-  return new Response(pngBuffer, {
-    headers: {
-      "content-type": "image/png",
-    },
-  });
-};
+//   return new Response(pngBuffer, {
+//     headers: {
+//       "content-type": "image/png",
+//     },
+//   });
+// };
